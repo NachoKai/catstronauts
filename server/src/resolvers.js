a6277c3545b9486605dd44a5d8264c67a4efa1b4
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-02-05 13:16:18 -0300

    add mutation to schema + add useMutation hook to the client

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,6 +6,31 @@ const resolvers = {
     track: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getTrack(id);
     },
+    module: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getModule(id);
+    },
+  },
+
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
   },
 
   Track: {
--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,18 @@ const typeDefs = gql`
   type Query {
     tracksForHome: [Track!]!
     track(id: ID!): Track!
+    module(id: ID!): Module!
+  }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    code: Int!
+    success: Boolean!
+    message: String!
+    track: Track
   }
 
   type Track {
@@ -28,6 +40,8 @@ const typeDefs = gql`
     id: ID!
     title: String!
     length: Int
+    content: String
+    videoUrl: String
   }
 `;
 
